bbe0a45dac3b1d849a2487f59f167fa3a8fedc2d
singh-amrita001 <137421187+singh-amrita001@users.noreply.github.com>
2026-02-06 03:39:09 -0500

    Add files via upload

---
+++ ItemapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amrita.itemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
